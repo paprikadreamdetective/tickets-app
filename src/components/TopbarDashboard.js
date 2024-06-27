08026a66ef5df12b3426acd7048375c3aa1ed592
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-06-27 10:32:11 -0600

    add top bar

--- src/components/TopbarDashboard.js
+++ src/components/TopbarDashboard.js
@@ -69,36 +69,3 @@ function TopbarDashboard() {
   );
 }
 export default TopbarDashboard;
-
-{/*<div className="row">
-        <div className="row top_tiles">
-            <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-24">
-                <div className="tile-stats">
-                    <div className="icon"><i className="fa fa-ticket"></i></div>
-                    <div className="count">0</div>
-                    <h3>Tickets Pendientes</h3>
-                </div>
-            </div>
-            <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-24">
-                <div className="tile-stats">
-                    <div className="icon"><i className="fa fa-list-alt"></i></div>
-                    <div className="count">0</div>
-                    <h3>Proyectos</h3>
-                </div>
-            </div>
-            <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-24">
-                <div className="tile-stats">
-                    <div className="icon"><i className="fa fa-th-list"></i></div>
-                    <div className="count">0</div>
-                    <h3>Categorias</h3>
-                </div>
-            </div>
-            <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-24">
-                <div className="tile-stats">
-                    <div className="icon"><i className="fa fa-users"></i></div>
-                    <div className="count">0</div>
-                    <h3>Usuarios</h3>
-                </div>
-            </div>
-        </div>
-    </div>*/}
